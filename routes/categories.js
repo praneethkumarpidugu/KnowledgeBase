70a1bd01a828f8b98476c730e6ca0374aaed2d87
praneethkumarpidugu <pidugu2007@gmail.com>
2016-08-08 22:28:29 -0400

    initial commit express server up and running

---
+++ routes/categories.js
@@ -0,0 +1,12 @@
+/**
+ * Created by praneethkumar on 08/08/16.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+module.exports = router;
