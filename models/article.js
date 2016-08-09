1c9963b27176db6fb2d57e972167c154306af071
praneethkumarpidugu <pidugu2007@gmail.com>
2016-08-08 23:24:47 -0400

    Schema for articles in both backend mongoshell and express server

---
+++ models/article.js
@@ -0,0 +1,47 @@
+/**
+ * Created by praneethkumar on 08/08/16.
+ */
+
+var mongoose = require('mongoose');
+
+var articleSchema = mongoose.Schema({
+    title: {
+        type: String,
+        index: true,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        index: true,
+        required: true
+    },
+    date: {
+        type: date,
+        default: Date.now
+    }
+});
+
+var Article = module.exports = mongoose.model('Article', articleSchema);
+
+//Since we have module.exports we can the below outside the file.
+
+//Get All Articles
+module.exports.getArticles = function (callback) {
+    Article.find(callback);
+}
+
+//Get Article by ID
+module.exports.getArticleById = function (id, callback) {
+    Article.findById(id, callback);
+}
+
+//Get Category Articles
+module.exports.getArticleByCategory = function (category, callback) {
+    var query = {category: category};
+    Article.find(query, callback);
+}
+
---
+++ models/category.js
@@ -0,0 +1,3 @@
+/**
+ * Created by praneethkumar on 08/08/16.
+ */
